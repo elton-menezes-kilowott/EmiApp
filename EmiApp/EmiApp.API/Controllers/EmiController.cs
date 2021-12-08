902e9f48ab620bc2a78698d90dfcd949f527190d
Elton De Menezes <92842347+elton-menezes-kilowott@users.noreply.github.com>
2021-12-08 11:57:50 +0530

    Add files via upload

---
+++ EmiApp/EmiApp.API/Controllers/EmiController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmiApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/[controller]/CalculateMontlyEmi")]
+        public Response CalculateMonthlyEmi(Request request)
+        {
+            ICalculator emiCalculator = new Calculator();
+
+            var response = new Response();
+
+            var emiMonthly = emiCalculator.CalculateMonthlyEmi(request);
+            return emiMonthly;
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/CalculateContinousEmi")]
+        public Response CalculateContinousEmi(Request request)
+        {
+            ICalculator emiCalcualtor = new Calculator();
+
+            var response = new Response();
+
+            var emiContinous = emiCalcualtor.CalculateContinousEmi(request);
+            return emiContinous;
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/CalculateDailyEmi")]
+        public Response CalculateDailyEmi(Request request)
+        {
+            ICalculator emiCalcualtor = new Calculator();
+
+            var response = new Response();
+
+            var emiDaily = emiCalcualtor.CalculateDailyEmi(request);
+            return emiDaily;
+        }
+    }
+}
